1886cb0e1c43801d0a2aa1fe384e884a7938bce5
HREngCode <ccrsailors@gmail.com>
2022-06-14 12:55:37 -0500

    Add Main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+
+shoe = {
+    "brand": "Nike",
+    "wide": False,
+    "color": "Black",
+    "shoe_size": 12,
+    "string_color" : ["orange", "blue", "purple"]
+}
+
+print(shoe["shoe_size"])
+
+x = shoe["brand"]
+print(x)
+
+x = shoe.keys()
+
+print(x)
+
+shoe["tongue_color"] = "black"
+
+print(x)
